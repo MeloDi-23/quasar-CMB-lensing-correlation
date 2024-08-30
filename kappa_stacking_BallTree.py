a0381c10626c3339464d6fbebd48e03801832e15
MeloDi-23 <hedi123@mail.ustc.edu.cn>
2024-08-30 15:45:10 -0600

    calculate the clustering of quasar

--- kappa_stacking_BallTree.py
+++ kappa_stacking_BallTree.py
@@ -1,20 +1,17 @@
-import healpy as hp
-import matplotlib.pyplot as plt
 import numpy as np
-from astropy.io import fits
 import astropy.units as u
 import astropy.constants as const
 from multiprocessing import Pool, Pipe, Process
 import tqdm
 from astropy.cosmology import Planck18 as cosmos
 import astropy.coordinates as coo
-# from sklearn.neighbors import BallTree
-h = cosmos.H0.to(u.km/u.s/u.Mpc).value / 100
 import argparse
+from sklearn.neighbors import BallTree
+h = cosmos.H0.to(u.km/u.s/u.Mpc).value / 100
 """
 Use BallTree to calculate the result, prove to be consistent with hp.query, but seems faster.
 Supports command line and parameters.
-Must use the galaxy catalogue of format: .npy, with ra, dec, z, w(weight)
+Must use the galaxy catalogue of format: .npy, with ra, dec(or l, b), z, w(weight)
 Must use the kappa map of format: .npy, with l, b, kappa
 """
 ### ========================================================== ###
@@ -25,31 +22,35 @@ parser.add_argument('--sep_min', '-m', default=3, help='min seperation [cMpc/h]'
 parser.add_argument('--sep_max', '-M', default=100, help='max seperation [cMpc/h]')
 parser.add_argument('--nbins', '-N', default=10, type=int, help='number of bins')
 parser.add_argument('--name', '-n', help='output name')
-parser.add_argument('--frame', '-f', help='frame of coordinats for catalogue file', default='icrs', choices=['galactic', 'icrs'])
-parser.add_argument('--unit', '-u', help='unit of catalogue file', default='deg')
+parser.add_argument('--frame', '-f', default='icrs', choices=['galactic', 'icrs'], help='frame of coordinats for catalogue file')
+parser.add_argument('--unit', '-u', default='deg', help='unit of catalogue file')
+parser.add_argument('--process', '-p', default=60, type=int, help='number of process')
 arg = parser.parse_args()
 
 if not arg.name:
     import os
     file_name = os.path.split(arg.file)[1]
-    arg.name = os.path.splitext(file_name)[0] + '_' + os.path.splitext(os.path.split(arg.CMB_file)[1])[0]
+    cmb_file_name = os.path.split(arg.CMB_file)[1]
+    arg.name = f'result_r={arg.sep_min}_{arg.sep_max}_{arg.nbins}_{os.path.splitext(file_name)[0]}_{os.path.splitext(cmb_file_name)[0]}_tree'
 ### ========================================================== ###
 
 # load the Planck kappa map
 print(f'loading {arg.CMB_file}...')
 l_k, b_k, kappa = np.load(arg.CMB_file)
 
-from sklearn.neighbors import BallTree
 tree = BallTree(data=np.vstack((b_k, l_k)).T, leaf_size=5, metric='haversine')          # latitude + logtitude
 
 print('finish loading Planck kappa map.')
 
 ### ========================================================== ###
 print(f'loading {arg.file}...')
+print(f'use frame {arg.frame}, unit {arg.unit}')
 catalogue = np.load(arg.file)
 Nquas = len(catalogue)
-c = coo.SkyCoord(l=catalogue['l'], b=catalogue['b'], frame=arg.frame, unit=arg.unit)
-
+if arg.frame=='icrs':
+    c = coo.SkyCoord(ra=catalogue['ra'], dec=catalogue['dec'], frame=arg.frame, unit=arg.unit)
+else:
+    c = coo.SkyCoord(l=catalogue['l'], b=catalogue['b'], frame=arg.frame, unit=arg.unit)
 l = c.galactic.l.to(u.rad).value
 b = c.galactic.b.to(u.rad).value
 # position of each quasar
@@ -61,22 +62,24 @@ print('finish loading catalogue.')
 
 ### ========================================================== ###
 
-Npro = 60
-Njack = 100
+Npro = arg.process
+print(f'use {Npro} process(es).')
 Nbins = arg.nbins
 sep_min = float(arg.sep_min)
 sep_max = float(arg.sep_max)
-name = arg.name
 r_bins = np.geomspace(sep_min, sep_max, Nbins+1)        # unit: cMpc/h
 
-output = f'./calculation_data/result_r={arg.sep_min}_{arg.sep_max}_{Nbins}_{name}_tree.npy'
+output = f'./calculation_data/{arg.name}.npy'
 print(f'output path {output}')
 
-sigma_frac = (const.c*const.c/(4*np.pi*const.G)).to(u.Msun*u.Mpc/u.pc/u.pc).value / h
-z_s = 1100      # redshift of CMB
-chi_s = coo.Distance(z=z_s, cosmology=cosmos).to(u.Mpc).value/(1+z_s)         # comoving distance of CMB
+sigma_frac = (const.c*const.c/(4*np.pi*const.G)).to(
+        u.Msun*u.Mpc/u.pc/u.pc).value / h
+z_s = 1100                                              # redshift of CMB
+chi_s = coo.Distance(z=z_s, cosmology=cosmos).to(
+    u.Mpc).value/(1+z_s)                            # comoving distance of CMB
 
 sender, receiver = Pipe()
+
 
 def stack_single_sample(arg):
     global sender
@@ -99,8 +102,9 @@ def stack_single_sample(arg):
     theta_bins = r_bins/h/(1+z_l)/d_A
     # convert from cMpc to theta
 
+    idx_all = tree.query_radius([[dec, ra]]*(Nbins+1), theta_bins)
     for i in range(Nbins):
-        idx_in, idx_out = tree.query_radius([[dec, ra], [dec, ra]], [theta_bins[i], theta_bins[i+1]])
+        idx_in, idx_out = idx_all[i], idx_all[i+1]
         idx = np.zeros(len(kappa), bool)
         idx[idx_out] = True
         idx[idx_in] = False
@@ -122,6 +126,7 @@ def stack_single_sample(arg):
 
     return values, weights
 
+
 def progress_bar(receiver, rate=10):
     # rate is the update rate of the progress bar.
     rate = max(1, int(rate))
@@ -136,13 +141,14 @@ def progress_bar(receiver, rate=10):
                 break
         pbar.close()
 
+
 mypool = Pool(processes=Npro)
 
 args = np.vstack((l, b, w_l, z_l)).T
 assert args.shape == (Nquas, 4)
 print('start calculation...')
 
-progress = Process(target=progress_bar, args=(receiver,100))
+progress = Process(target=progress_bar, args=(receiver, 100))
 progress.start()
 
 result = mypool.map(stack_single_sample, args, chunksize=200)
@@ -152,11 +158,10 @@ progress.join()
 
 values = np.vstack(tuple(item[0] for item in result))
 weight = np.vstack(tuple(item[1] for item in result))
-assert values.shape==weight.shape==(Nquas, Nbins)
+assert values.shape == weight.shape == (Nquas, Nbins)
 
 print('finish calculating.')
 print('completeness: {}.'.format(1-np.sum(np.isnan(values))/(Nquas*Nbins)))
 
 print(f'save to {output}...')
 np.save(output, (values, weight))
-
